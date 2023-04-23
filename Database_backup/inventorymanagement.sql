3b573e8241bcf8a13f159d8e8fdca60c67cfdd93
Jazmine Joaquin <99586254+morii21@users.noreply.github.com>
2023-04-23 20:46:41 +0800

    Update inventorymanagement.sql

--- Database_backup/inventorymanagement.sql
+++ Database_backup/inventorymanagement.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 23, 2023 at 02:37 PM
+-- Generation Time: Apr 23, 2023 at 02:46 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.0.25
 
